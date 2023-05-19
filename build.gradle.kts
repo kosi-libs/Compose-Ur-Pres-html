c24de4505f1d5615f9a72e217ee9230875b232e6
Salomon BRYS <salomon.brys@gmail.com>
2023-05-19 13:58:03 +0200

    Demo built for GH Pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,10 @@ allprojects {
     group = "org.kodein.compose.html.pres"
     version = "1.8.0"
 }
+
+task<Sync>("updateDemoDocs") {
+    group = "publishing"
+    dependsOn("demo:jsBrowserDistribution")
+    from("$rootDir/demo/build/distributions")
+    into("$rootDir/docs/demo")
+}
